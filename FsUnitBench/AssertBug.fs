1fe09aad3b3170b775b44c5e5c2bcc6a57e73fa1
cboudereau <clement.boudereau@availpro.com>
2020-04-27 11:10:29 +0200

    clean

--- FsUnitBench/AssertBug.fs
+++ FsUnitBench/AssertBug.fs
@@ -5,8 +5,5 @@ open Xunit
 
 module AssertBug =
     
-    let [<Fact>] ``result : actual message not exact, the code compiles even if types aren't compatible`` () =
-        let actual = Ok "1J"
-        let expected = Ok "1P"
-
-        actual |> should equal expected
+    let [<Fact>] ``result bug`` () =
+        Ok "hello" |> should equal (Ok "world")
